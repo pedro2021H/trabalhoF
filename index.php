5c32a4a1f1bea8bb966d10e31ca9ba4255c4818a
NovaisDev <145157972+NovaisDev@users.noreply.github.com>
2023-09-29 16:45:51 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <title>LOGIN</title>
+</head>
+<body>
+    <form action="" method="POST">
+      <h1> acesse sua conta </h2>
+    <p>
+      <label>E-mail</label>
+      <input type="password" name="email">      
+    </p>
+    <p>
+      <label>Senha</label>
+      <input type="passowrd" name="senha">
+</p>
+      <button>Login </button>
+
+      
+     
+</body>
+</html>
